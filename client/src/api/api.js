54db5e74bce602d6a33b9baf51bbab320d5e4cba
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-04-26 17:17:57 +0530

    create some frontend fetures.

---
+++ client/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// http://localhost:8080/api/
+const API = axios.create({
+    baseURL: "http://localhost:8080/api/"
+});
+
+export const GetPosts = async () => await API.get("/post/");
+export const CreatePost = async () => await API.post("/post/", data);
+export const GenerateImageFromPrompt = async (data) => await API.post("/generateImage/", data);
