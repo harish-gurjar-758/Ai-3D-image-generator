40057975281df121204a58a3f06e7f781e158a8f
harish-gurjar-758 <gurjarharish758@gmail.com>
2025-04-27 18:48:57 +0530

    some changes in code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
-import './App.css';
+import styled, { ThemeProvider, themeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./utils/theme";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import Home from './pages/Home';
+import CreatePost from "./pages/CreatePost";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/post" exact element={<CreatePost />} />
+            </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
   );
 }
 
